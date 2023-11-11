06a6820f8890bb668d74000dc2453810c552788e
Steele Russell <sarxyz@users.noreply.github.com>
2023-11-10 23:58:32 -0600

    Update Neptune.java
    fixed Neptune

--- src/Neptune.java
+++ src/Neptune.java
@@ -1,16 +1,7 @@
 public class Neptune {
     /* Attributes/Data fields/Properties */
     // YOUR CODE HERE
-    public static long triton(long index){
-        if (index == 1)
-            return 1;
-        else if (index == 2)
-            return 2;
-        else if (index == 3)
-            return 3;
-        else
-            return triton(index -1) + triton(index - 2) + triton(index -3);
-    }
+    
     /* Constructor(s) */
     // YOUR CODE HERE
 
